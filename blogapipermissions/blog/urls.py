f97c141724c117ed1c4ad5a0f48cf9d4409cf8ab
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-05-26 13:44:15 +0300

    building postgresql

--- blogapipermissions/blog/urls.py
+++ blogapipermissions/blog/urls.py
@@ -4,5 +4,4 @@ from blog.api.viewsets import (ArticlesDetailView, ArticlesListView)
 urlpatterns = [
     path('articles-list', ArticlesListView.as_view(), name = 'articles_list'),
     path('article-detail/<int:pk>', ArticlesDetailView.as_view(), name = 'article_detail'),
-
 ]
